6f010643cd69e7256af9f0daf4b5c2fd30c385c5
JK <usinghuman@gmail.com>
2020-01-25 01:27:57 +0100

    Implemented ability to define global components

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# v-stash
-v-stash is a vue directive package that allows you to make data of components persist between sessions, pages and so on.
+# Vue-stasher
+> Implement data persistence with ease
+
+### Installation
+
+To install the package, run the follow ing command:
+
+`npm i vue-stasher`
+
+To use package in your component or the entire app, use the code below:
+
+```
+import VueStasher from 'vue-stasher'
+```
+
+### Usage
+
+To make data of your component persistent, simply add `v-stash` directive to it, here's an example:
+
+```html
+<v-toggle-button v-stash="{ properties: ['toggled'] }">
+    My state is: {{ toggled }}
+</v-toggle-button>
+```
+
+Note: `properties` defines fields inside `data()` that are meant to be stored.
+
+### Global components
+
+If you have a component that's meant to retain it's values regardless of the page it's displaying on **(components like menus, search bars, notifications etc.)** - simply add `identifier: "my-unique-name"` to object returned by the directive.
+
+Example:
+
+```html
+<v-menu v-stash="{ identifier: 'menu', properties: ['collapsedSections'] }">
+    ...
+</v-menu>
+```
---
+++ src/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+
+// fields:
+//  properties: values from data to store
+//  global: set this to true if state of your component is shared between pages (elements like nav, menu, search ..), otherwise identifiers will be prepended with the page url
+
+export default Vue.directive('stash',
+{
+    inserted(el, binding, vnode)
+    {
+        var instance = vnode.componentInstance || (vnode.parent ? vnode.parent.componentInstance : null)
+
+        if(!instance)
+            console.warn('v-stash directive has to be placed on a component or a topmost child inside of the template!')
+
+        var properties = binding.value.properties
+        var identifier = 'stashable-' + (binding.value.identifier || document.location.pathname.replace('/', '-') + '-id-' + instance._uid)
+        var stashed = JSON.parse(localStorage.getItem(identifier))
+
+        console.log(identifier)
+
+        for (var i = 0; i < properties.length; i++)
+        {
+            var property = properties[i]
+
+            if(stashed)
+            {
+                instance[property] = stashed[property]
+            }
+
+            instance.$watch(property, function(n, o)
+            {
+                if(n != o)
+                {
+                    var stash = {}
+
+                    for (var i = 0; i < properties.length; i++)
+                    {
+                        stash[properties[i]] = instance[properties[i]]
+                    }
+
+                    localStorage.setItem(identifier, JSON.stringify(stash))
+                }
+            }, { deep: true})
+        }
+    }
+})
