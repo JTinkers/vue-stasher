23fa59fb0fd53cbe2900726401f93e6bb938a566
JK <usinghuman@gmail.com>
2020-01-26 12:30:28 +0100

    Changes to how identifiers are generated
    - Storage ids will now be even more unique, to prevent stasher from trying to apply data of component of type A to component of type B

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 
-// fields:
-//  properties: values from data to store
-//  global: set this to true if state of your component is shared between pages (elements like nav, menu, search ..), otherwise identifiers will be prepended with the page url
-
 export default Vue.directive('stash',
 {
     inserted(el, binding, vnode)
@@ -14,7 +10,7 @@ export default Vue.directive('stash',
             console.warn('v-stash directive has to be placed on a component or a topmost child inside of the template!')
 
         var properties = binding.value.properties
-        var identifier = 'stashable-' + (binding.value.identifier || document.location.pathname.replace('/', '-') + '-id-' + instance._uid)
+        var identifier = 'stashable-' + (binding.value.identifier || document.location.pathname.replace('/', '-') + '-' + instance.$vnode.tag + '-id-' + instance._uid)
         var stashed = JSON.parse(localStorage.getItem(identifier))
 
         for (var i = 0; i < properties.length; i++)
